884e3106478c25cda256dac1a3cb7a5a3d6433a6
takamt <tkn.moto@gmail.com>
2025-07-13 18:38:29 +0900

    refactor: fix relative path calculation in get_relative_path_from_home function

--- install.sh
+++ install.sh
@@ -61,7 +61,7 @@ ensure_directory() {
 get_relative_path_from_home() {
     path="$1"
     # HOMEディレクトリからの相対パスを計算
-    relative_path="$(realpath --relative-to="$HOME" "$path")"
+    relative_path="$(realpath -m --relative-to="$HOME" "$path")"
     echo "$relative_path"
 }
 

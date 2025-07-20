50f587165d5ebbaf7a058648a045ce457ec6e30f
takamt <tkn.moto@gmail.com>
2025-07-21 02:24:14 +0900

    refactor: Add sudo to package management commands in install-zsh.sh

--- scripts/install-zsh.sh
+++ scripts/install-zsh.sh
@@ -34,12 +34,12 @@ curl -o \
 # Install eza instead of ls
 sudo apt-get install -y \
     gpg \
-    && mkdir -p /etc/apt/keyrings \
-    && wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | gpg --dearmor -o /etc/apt/keyrings/gierens.gpg \
-    && echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | tee /etc/apt/sources.list.d/gierens.list \
-    && chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list \
-    && apt-get update \
-    && apt-get install --no-install-recommends -y \
+    && sudo mkdir -p /etc/apt/keyrings \
+    && wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg \
+    && echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list \
+    && sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list \
+    && sudo apt-get update \
+    && sudo apt-get install --no-install-recommends -y \
     eza
 
 
